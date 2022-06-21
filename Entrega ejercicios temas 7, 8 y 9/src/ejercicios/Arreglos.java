ebb5ee8b92ef07edaa300a9b8c1cfce149a5a8c6
Familia <Familia@Usuario-PC>
2022-06-20 21:37:24 -0500

    Entrega trabajos 7,8,9

---
+++ Entrega ejercicios temas 7, 8 y 9/src/ejercicios/Arreglos.java
@@ -0,0 +1,22 @@
+package ejercicios;
+
+public class Arreglos {
+	int i=0;
+	String[] unaDimension;{
+	unaDimension=new String[5];
+	unaDimension[0]="Juan";
+	unaDimension[1]="Pedro";
+	unaDimension[2]="Samuel";
+	unaDimension[3]="Karla";
+	unaDimension[4]="Ximena";
+	for(int i=0; i<unaDimension.length; i++) {
+		System.out.println(unaDimension[i]);
+	}
+	
+	
+}
+	public String toString(){
+		return unaDimension[i] ;
+	}
+	
+}
